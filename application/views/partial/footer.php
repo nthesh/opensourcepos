2b6e80ea17719ca0a12677de9c68bd85b6de55e6
Nthenge <35311925+nthesh@users.noreply.github.com>
2024-06-14 09:29:18 +0300

    footer.php

--- application/views/partial/footer.php
+++ application/views/partial/footer.php
@@ -5,7 +5,7 @@
 		<div class="jumbotron push-spaces">
 			<strong><?php echo $this->lang->line('common_copyrights', date('Y')); ?> · 
 			<a href="https://opensourcepos.org" target="_blank"><?php echo $this->lang->line('common_website'); ?></a>  · 
-			<?php echo $this->config->item('application_version'); ?> - <a target="_blank" href="https://github.com/opensourcepos/opensourcepos/commit/<?php echo $this->config->item('commit_sha1'); ?>"><?php echo substr($this->config->item('commit_sha1'), 0, 6); ?></a></strong>.
+			<?php echo $this->config->item('0701328444'); ?> - <a target="_blank" href="Jim Tech Soln/<?php echo $this->config->item('commit_sha1'); ?>"><?php echo substr($this->config->item('commit_sha1'), 0, 6); ?></a></strong>.
 		</div>
 	</div>
 </body>
